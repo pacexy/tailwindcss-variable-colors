dc649de17cc358d907853ddf06ffe694260dde10
pacexy <pacexy@gmail.com>
2023-04-29 10:43:01 +0800

    chore(site): demo page

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-white">
       <body className={inter.className}>{children}</body>
     </html>
   )
--- site/tailwind.config.ts
+++ site/tailwind.config.ts
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+import {
+  createVariableColors,
+  defineColorVariables,
+} from 'tailwindcss-variable-colors'
+
+const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: createVariableColors(colors),
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -13,6 +20,12 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
   },
-  plugins: [],
+  plugins: [defineColorVariables({ colors })],
 }
+
+export default config
